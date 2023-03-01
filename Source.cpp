71c4f27449aa2e45d0bef88dff5db04e88ec6add
Eti-C <friendlyrabbit@gmail.com>
2023-03-01 01:07:49 -0800

    with input validation

--- Source.cpp
+++ Source.cpp
@@ -16,9 +16,13 @@ pizza price is a dollars and cents value input as raw input
 warning the price of the pizza can include micropennies if you accidentally put three or more digits in cents
 pizza deal is the pizza area divided by the pizza price
 
+
+the next step:
+error handling. sometime after that socket code
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 float pizza_calculation(float pizza_diameter)
 {
 	float pizza_radius = (pizza_diameter / 2);
@@ -36,21 +40,54 @@ float price_per_square_inch(float dollar_and_cents_price, float area_sq_inch)
 int main(void)
 {
 	float pizza_diameter = 0;
-	printf("input the diameter of your pizza in inches: ");
-	scanf_s("%f", &pizza_diameter);
+
+//pasted code that we will try to work with
+	char input[100];
+
+
+
+/*	int i;
+
+	printf("Enter input: ");
+	fgets(input, sizeof(input), stdin);
+
+	for (i = 0; input[i] != '\0'; i++) {
+		if (isalpha(input[i])) {
+			printf("Invalid input: Contains letters\n");
+			return 1;
+		}
+*/
+	while (pizza_diameter <= 0)
+	{
+
+
+		printf("input the diameter of your pizza in inches: ");
+
+		scanf_s("%s", input, (unsigned)_countof(input));
+		sscanf_s(input, "%f", &pizza_diameter);
+	
+	}
+
 	float Calculated_pizza_area = pizza_calculation(pizza_diameter);
 	printf("your pizza is %f square inches \n", Calculated_pizza_area);
 
 
-	float pizza_price;
-	printf("input the pizzas price: ");
-	scanf_s("%f", &pizza_price);
+	float pizza_price = -1;
+	
+	
+	while (pizza_price < 0)
+	{
+
+		printf_s("please input the price: ");
+		scanf_s("%s", input, (unsigned)_countof(input));
+		sscanf_s(input, "%f", &pizza_price);
+	}
 	
 	float spending_per_square_inch = price_per_square_inch(pizza_price, Calculated_pizza_area);
 	printf("the amount of money you are paying per square inch of pizza is $%f \n", spending_per_square_inch);
 
 	int no_input_no_close = 0;
-	scanf_s("%d", no_input_no_close);
+	scanf_s("%d", &no_input_no_close);
 
 	return(0);
 
